a1f0b468084fa3c33cd45c586172ef9d0a67e705
hosua <hosua.o@gmail.com>
2025-12-12 09:09:30 -0500

    implement sentContactEmail lambda

---
+++ lambdas/contactForm/sendContactEmail.mjs
@@ -0,0 +1,82 @@
+import { SESv2Client, SendEmailCommand } from "@aws-sdk/client-sesv2"; // ES Modules import
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+} from "@aws-sdk/lib-dynamodb";
+const { region, table_name, from_email, my_email } = process.env;
+
+const sesClient = new SESv2Client({});
+
+const dbClient = new DynamoDBClient({ region });
+const db = DynamoDBDocumentClient.from(dbClient);
+
+const SIX_HOURS_IN_MS = 6 * 60 * 60 * 1000;
+
+export const handler = async (event) => {
+  const { email, body, ip } = event.body || {};
+
+  const getItem = new GetCommand({
+    TableName: table_name,
+    Key: {
+      ip,
+    },
+  });
+  const { Item } = await db.send(getItem);
+
+  if (!Item) {
+    const sendEmail = new SendEmailCommand({
+      FromEmailAddress: from_email,
+      Destination: {
+        ToAddresses: [my_email],
+      },
+      Content: {
+        Simple: {
+          Subject: {
+            Data: `Contacrt form submissions from ${email}`,
+          },
+          Body: {
+            Text: {
+              Data: `From: ${email}\n\n${body}`,
+            },
+          },
+        },
+      },
+    });
+    await sesClient.send(sendEmail);
+
+    // Insert to recent senders table to prevent spam
+    const expire_at = Math.floor(
+      (new Date().getTime() + SIX_HOURS_IN_MS) / 1000,
+    );
+    const putItem = new PutCommand({
+      TableName: table_name,
+      Item: {
+        ip,
+        expire_at,
+      },
+    });
+
+    await db.send(putItem);
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: `Successfully sent email` }),
+    };
+  } else {
+    // User recently sent an email, block the request
+    return {
+      statusCode: 420,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        error:
+          "This IP address has sent an email recently, please wait 6 hours before sending another one!",
+      }),
+    };
+  }
+};
