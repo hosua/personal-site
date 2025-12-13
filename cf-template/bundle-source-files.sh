9f4d85174c3a3375fb18a66d4dbf02f9c507f1cd
hosua <hosua.o@gmail.com>
2025-12-13 03:50:39 -0500

    This gonna be a lot of work if we do it...

---
+++ cf-template/bundle-source-files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# TODO: This is gonna be quite a bit of work. Maybe we should keep this simple
+# and try and only make a 1-click-deploy for the URL Shortener?
+
+# packages the frontend
+
+# upload to some s3 bucket
+
+# package the lambdas
+
+# upload to some s3 bucket
+
+# modify CF template to reference s3 files
